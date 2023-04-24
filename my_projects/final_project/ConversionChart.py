1d091d730cb852769e8ce1704dba3da82b1363bf
WattersClark <clarkwatters30@gmail.com>
2023-04-24 18:06:54 +0000

    Final project improvements

--- my_projects/final_project/ConversionChart.py
+++ my_projects/final_project/ConversionChart.py
@@ -12,15 +12,20 @@ def cm_to_inches(cm):
     inches = cm * 0.393701
     return inches
 
+def kg_to_lbs(kg):
+    lbs = kg * 2.2
+    return lbs
+
 def main():
     option = 0
-    print("Oy Bruv, make your choice: ")
+    print("Oi Bruv, make your bleedin choice, innit?: ")
     while True:
         print("1. Celsius to Fahrenheit")
         print("2. Pound Sterling to USD")
         print("3. Cm to Inches")
-        print("4. Exit the conversion")
-        option = int(input("> (1/2/3/4): "))
+        print("4. Kg to lbs")
+        print("5. Exit the conversion")
+        option = int(input("> (1/2/3/4/5): "))
 
         if option == 1:
             celsius = float(input("Enter temperature in Celsius: "))
@@ -35,11 +40,14 @@ def main():
             inches = cm_to_inches(cm)
             print(f"\n{cm} cm is equal to {inches} inches.\n")
         elif option == 4:
+            kg = float(input("Enter weight in kg: "))
+            lbs = kg_to_lbs(kg)
+            print(f"\n{kg} kg is equal to {lbs} lbs.\n")
+        elif option == 5:
             break
         else:
             print("That is not a choice.")
-    print("Cheers")
+    print("Cheers Guvna")
 
 if __name__ == '__main__':
     main()
-
